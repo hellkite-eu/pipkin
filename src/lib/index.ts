057614966d9fcc67b13607c9c06ff9d7734d81cc
KennyTheBard <octav_teculescu@yahoo.com>
2025-04-03 13:27:30 +0300

    Fix module

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export * from './template';
+export * from './bundler';
+export * from './replacement';
+export * from './types';
+export * from './utils';
---
+++ src/lib/utils/index.ts
@@ -0,0 +1,6 @@
+export * from './buildFontString';
+export * from './canvasToImage';
+export * from './container';
+export * from './drawBoundingBox';
+export * from './enhanceTextbox';
+export * from './placeImage';
